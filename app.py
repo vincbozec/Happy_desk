083ed07f7cc665e5c4f641d50b69b890d85e95ac
vincbozec <vincent.bozec@supdevinci-edu.fr>
2024-04-03 16:56:20 +0200

    test modif du code

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def home():
 def new_deployment():
   return "New deployment from CI/CD"
 
+@app.route("/info")
+def info():
+  user_agent = request.headers.get('User-Agent')
+  return f"Your user agent is: {user_agent}"
+
 
 if __name__ == "__main__":
   app.run(debug=True)
